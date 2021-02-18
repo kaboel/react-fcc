0d08bb4e80c960adf235488d336606d625a0c881
Faiq <faiq.kaboel@gmail.com>
2021-02-19 01:02:36 +0700

    add book form

--- booklistapp/src/components/BookForm/index.js
+++ booklistapp/src/components/BookForm/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { BookContext } from "../../contexts/BookContext";
 
 const BookForm = () => {
-  return <div></div>;
+  const { addBook } = useContext(BookContext);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle("");
+    setAuthor("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Book title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Book Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        required
+      />
+
+      <input type="submit" value="Add Book" />
+    </form>
+  );
 };
 
 export default BookForm;
