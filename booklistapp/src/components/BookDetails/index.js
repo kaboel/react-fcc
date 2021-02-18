13af3bc5579be90e4598d6a0597356304f433c52
Faiq <faiq.kaboel@gmail.com>
2021-02-19 01:31:56 +0700

    use reducer for data mutation

--- booklistapp/src/components/BookDetails/index.js
+++ booklistapp/src/components/BookDetails/index.js
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import { BookContext } from "../../contexts/BookContext";
 
 const BookDetails = ({ book }) => {
-  const { removeBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
 
   return (
-    <li onDoubleClick={() => removeBook(book.id)}>
+    <li onDoubleClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}>
       <div className="title">{book.title}</div>
       <div className="author">{book.author}</div>
     </li>
--- booklistapp/src/contexts/BookContext/index.js
+++ booklistapp/src/contexts/BookContext/index.js
@@ -1,33 +1,13 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
+import { bookReducer } from "../../reducers/BookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
-    { title: "the way of kings", author: "brandon sanderson", id: 2 },
-    { title: "the final empire", author: "patrick rothfuss", id: 3 },
-    { title: "the hero of ages", author: "brandon sanderson", id: 4 },
-    { title: "songs of ice and fire", author: "george r.r. martin", id: 5 },
-  ]);
-
-  const addBook = (title, author) => {
-    setBooks([
-      ...books,
-      {
-        title: title,
-        author: author,
-        id: books[books.length - 1].id + 1,
-      },
-    ]);
-  };
-
-  const removeBook = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
+  const [books, dispatch] = useReducer(bookReducer, []);
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
